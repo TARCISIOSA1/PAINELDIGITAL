9f61e45042ef6fad43708379f180dffbde7719a5
TARCISIOSA1 <TARCISIO.SA1@GMAIL.COM>
2025-07-08 12:35:32 -0300

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,3 +1,4 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; // Firestore
 import { getStorage } from "firebase/storage";      // Storage
@@ -6,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBng0-cT_YgFL_E0nUOJzprxv0ijkhIedk",
   authDomain: "camaravotacao.firebaseapp.com",
   projectId: "camaravotacao",
-  storageBucket: "camaravotacao.appspot.com",
+  storageBucket: "camaravotacao.firebasestorage.app", // <-- CORRIGIDO AQUI!
   messagingSenderId: "54469108444",
   appId: "1:54469108444:web:2448b08bb7c9c723f60a19"
 };
